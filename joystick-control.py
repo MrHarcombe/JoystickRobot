5339103f0a2cf774c027ff7205398e89fcc90a15
Ian Harcombe <ian.harcombe@gmail.com>
2016-03-23 20:42:56 +0000

    Added logging back in, so I can start looking at the values coming through and try and fine-tune the values for the motors calculated from the current joystick co-ordinates.

--- joystick-control.py
+++ joystick-control.py
@@ -10,8 +10,11 @@ keypress_actions = {
     ecodes.ABS_Y: store_y
 }
 
-current_y = 0
 current_x = 0
+current_y = 0
+
+power_l = 0
+power_r = 0
 
 def store_x(value):
     current_x = value;
@@ -27,14 +30,18 @@ def limit_value(value):
     return value
 
 def update_motor_powers():
-    robot.left (limit_value((current_y / 32768 * 100) + (current_x / 32768 * 100)))
-    robot.right(limit_value((current_y / 32768 * 100) - (current_x / 32768 * 100)))
+    power_l = limit_value((current_y / 32768 * 100) + (current_x / 32768 * 100))
+    power_r = limit_value((current_y / 32768 * 100) - (current_x / 32768 * 100))
+    robot.left (power_l)
+    robot.right(power_r)
 
 try:
-    #with open("values.txt", "w") as log:
+    #with open("values.log", "w") as log:
         for event in keyboard.read_loop():
             if event.code in keypress_actions:
+                #print(categorized, categorized.event.code, categorized.event.sec, categorized.event.timestamp, categorized.event.type, categorized.event.usec, categorized.event.value, file=log)
                 keypress_actions[event.code](event.value)
                 update_motor_powers()
+                #print("x:", current_x, "y:", current_y, "power_l:", power_l, "power_r:", power_r, file=log)
 except KeyboardInterrupt:
     robot.stop()
